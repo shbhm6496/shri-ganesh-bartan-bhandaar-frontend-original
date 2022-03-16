d9382638eadd8c04b6562959f380798bc29e1cdf
=shubham garg <=shbhm6496@gmail.com>
2022-03-16 12:55:35 +0530

    add product page

--- proshop-frontend/src/App.js
+++ proshop-frontend/src/App.js
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Shri Ganesh Bartan Bhandaar</h1>
+          <HomePage />
         </Container>
       </main>
       <Footer />
