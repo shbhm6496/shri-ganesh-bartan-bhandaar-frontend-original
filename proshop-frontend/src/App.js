f8fe768335ce378487c93b1f49b06b9eb8cc5da9
=shubham garg <=shbhm6496@gmail.com>
2022-03-16 14:43:09 +0530

    add react router

--- proshop-frontend/src/App.js
+++ proshop-frontend/src/App.js
@@ -3,17 +3,23 @@ import Header from "./components/Header";
 import { Container } from "react-bootstrap";
 import HomePage from "./pages/HomePage";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductPage from "./pages/ProductPage";
+
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            <Route path="/product/:id" element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
